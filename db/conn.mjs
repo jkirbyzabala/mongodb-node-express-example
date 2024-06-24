b41b7d91defc4dfa68618b65ce173101ad393404
Kirby Zabala <jkirbyzabala@gmail.com>
2024-06-24 09:01:25 -0500

    updates to conn.mjs file worked

--- db/conn.mjs
+++ db/conn.mjs
@@ -3,7 +3,7 @@
 import { MongoClient } from 'mongodb';
 
 
-const connectionString = process.env.ATLAS_URI || "mongodb+srv://jkirbyzabala:<password>@mongopractice.evnby4o.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=MongoPractice";
+const connectionString = process.env.ATLAS_URI || "mongodb+srv://jkirbyzabala:13@Car3fr33@mongopractice.evnby4o.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=MongoPractice";
 
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
